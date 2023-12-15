73aded24d2d708c8152bef22ea009f924a1ce994
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-15 01:10:30 +0100

    task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,8 +6,10 @@ CREATE TRIGGER valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF OLD.email != NEW.email then
+    IF NEW.email != OLD.email THEN
         SET NEW.valid_email = 0;
+    ELSE
+	SET NEW.valid_email = NEW.valid_email;
     END IF;
 END $$
 DELIMITER ;
