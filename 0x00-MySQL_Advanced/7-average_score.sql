266be1f49a59a8a42021e417f46d4859c4f363b9
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-15 01:29:46 +0100

    task 7

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,28 @@
+--  a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
+
+BEGIN
+	DECLARE total_score INT DEFAULT 0;
+	DECLARE total_projects INT DEFAULT 0;
+
+	SELECT SUM(score) INTO total_score
+	FROM corrections
+        WHERE corrections.user_id = user_id;
+
+        SELECT COUNT(*) INTO total_projects
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+
+	IF total_projects > 0 THEN
+		UPDATE users
+		SET users.average_score = total_score / total_projects
+		WHERE users.id = user_id;
+	END IF;
+END $$
+
+DELIMITER ;
