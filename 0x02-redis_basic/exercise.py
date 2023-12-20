4d9981e1b35dd4aaf38a56f9addfc0552fdafd38
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-20 20:52:44 +0100

    task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""write strings"""
+
+import redis
+import uuid
+from typing import Union
+
+class Cache:
+    """store instance od Redis"""
+    def __init__(self):
+        """Initialize the Cache Creates Redis client and clears the cache"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """store data in the cache and return a unique key"""
+        key = str(uuid.uuid4())
+
+        self._redis.set(key, data)
+
+        return key
